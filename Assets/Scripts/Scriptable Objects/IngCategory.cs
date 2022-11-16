33c479c9e13ba57155b3f06a90660c36ae149cca
RaymondScottSmith <rayscottsmith1@gmail.com>
2022-11-16 15:12:17 -0600

    Began work on Lab Level

--- Assets/Scripts/Scriptable Objects/IngCategory.cs
+++ Assets/Scripts/Scriptable Objects/IngCategory.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Ing_Category", menuName = "Scriptable Object / New Category")]
